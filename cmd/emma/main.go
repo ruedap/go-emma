1245aa52c730981d768b9c4cd37082a8858c4dac
ruedap <ruedap@ruedap.com>
2015-04-07 23:29:33 +0900

    Update cmd/emma/main.go

--- cmd/emma/main.go
+++ cmd/emma/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/ruedap/go-emma"
+import (
+	"fmt"
+	"os"
+
+	"github.com/ruedap/go-emma"
+)
 
 func main() {
-	emma.Find([]string{})
+	ret := emma.Find(emma.Src, os.Args[1:])
+	str, err := emma.ToJSON(ret)
+	if err != nil {
+		fmt.Println("Failed to output json.")
+	}
+
+	fmt.Println(str)
 }
